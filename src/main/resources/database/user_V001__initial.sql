9b270ea135f1bad8ff003c327a76250e1a6dfcea
dprol <d.prolisko05@gmail.com>
2025-05-31 14:56:22 +0300

    table

--- src/main/resources/database/user_V001__initial.sql
+++ src/main/resources/database/user_V001__initial.sql
@@ -7,5 +7,126 @@ CREATE TABLE users (
     active boolean default true NOT NULL,
     bio VARCHAR(512),
     createdAt timestamptz DEFAULT current_timestamp,
-    updateAt timestamptz DEFAULT current_timestamp
+    updateAt timestamptz DEFAULT current_timestamp,
+    profile_pic_file_id TEXT,
+    profile_pic_small_file_id TEXT,
+
+    CONSTRAINT fk_country_id FOREIGN KEY (country_id) REFERENCES country(id)
+)
+
+CREATE TABLE country (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    country_name VARCHAR(64) NOT NULL
+)
+
+CREATE TABLE Jira (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    url VARCHAR UNIQUE NOT NULL,
+    user_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (users_id) REFERENCES users(id)
+)
+
+CREATE TABLE Premium(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    user_id BIGINT UNIQUE NOT NULL,
+    start_date_time timestamptz DEFAULT current_timestamp,
+    end_date_time timestamptz,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (users_id) REFERENCES users(id)
+)
+
+CREATE TABLE Rating(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    rating INT,
+    user_id BIGINT UNIQUE NOT NULL,
+    created_datatime timestamptz DEFAULT current_timestamp,
+    updated_datatime timestmaptz DEFAULT current_timestamp,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE skills(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    skillName VARCHAR(32) NOT NULL,
+    createdAt timestampz DEFAULT current_timestamp,
+    updatedAt timestampz DEFAULT current_timestamp,
+    user_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE SkillGuarantee(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    user_id BIGINT UNIQUE NOT NULL,
+    userguarentee_id BIGINT UNIQUE NOT NULL,
+    skill_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_userguarantee_id FOREIGN KEY (userguarentee_id) REFERENCES users(id),
+    CONSTRAINT fk_skill_id FOREIGN KEY (skill_id) REFERENCES  users(id)
+)
+
+CREATE TABLE goal(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    title VARCHAR(32) NOT NULL,
+    description VARCHAR(128),
+    createdAt timestampz DEFAULT current_timestamp,
+    updateAt timestampz DEFAULT current_timestamp,
+    deadline timestampz,
+    status SMALLINT DEFAULT 0 NOT NULL,
+    user_id BIGINT UNIQUE NOT NULL,
+    goal_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_goal_id FOREIGN KEY (goal_id) REFERENCES users(id)
+)
+
+CREATE TABLE goal_invitation(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    goal_id BIGINT UNIQUE NOT NULL,
+    inviter_id BIGINT UNIQUE NOT NULL,
+    invited_id BIGINT UNIQUE NOT NULL,
+    createdAt timestampz DEFAULT current_timestamp,
+    updatedAT timestampz DEFAULT current_timestamp,
+    status SMALLINT DEFAULT 0 NOT NULL,
+
+    CONSTRAINT fk_goal_id FOREIGN KEY (goal_id) REFERENCES goal(id),
+    CONSTRAINT fk_inviter_id FOREIGN KEY (inviter_id) REFERENCES users(id),
+    CONSTRAINT fk_invited_id FOREIGN KEY (invited_id) REFERENCES users(id)
+)
+
+CREATE TABLE contact(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    contact VARCHAR(128) UNIQUE,
+    type SMALLINT DEFAULT 0 NOT NULL,
+    user_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE contact_preference(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    prefered_type SMALLINT DEFAULT 0 NOT NULL,
+    user_id BIGINT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE subscription(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    follower_id BIGINT NOT NULL ,
+    following_id BIGINT NOT NULL,
+    createdAt timestampz DEFAULT current_timestamp,
+    updatedAt timestampz DEFAULT current_timestamp,
+
+    CONSTRAINT fk_follower_id FOREIGN KEY (follower_id) REFERENCES users(id),
+    CONSTRAINT fk_following_id FOREIGN KEY (following_id) REFERENCES users(id)
+)
+
+CREATE TABLE ContentData(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    content OID
 )

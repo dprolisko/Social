470d41ef2c7c263ec2da94299e05085cd0740cb0
dprol <d.prolisko05@gmail.com>
2025-07-30 13:15:28 +0300

    Test2

---
+++ user_service/src/test/java/com/dprol/social/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dprol.social;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
